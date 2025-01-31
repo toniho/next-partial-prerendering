6ddea067fd2087558551238d10640fa2e55d3b4f
Toni Ho <tonih@zillowgroup.com>
2025-01-31 11:07:06 -0800

    add product page

---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,33 @@
+import { Suspense } from 'react';
+import {
+  RecommendedProducts,
+  RecommendedProductsSkeleton,
+} from '#/components/recommended-products';
+import { Reviews, ReviewsSkeleton } from '#/components/reviews';
+import { SingleProduct } from '#/components/single-product';
+import { Ping } from '#/components/ping';
+
+export const experimental_ppr = true;
+
+export default async function Page({ params }: { params: Promise<{ id: string }>}) {
+  const id = (await params).id;
+  console.log(id);
+  return (
+    <div className="space-y-8 lg:space-y-14">
+      <SingleProduct />
+      <h2 data-test-id="product-id">{id}</h2>
+
+      <Ping />
+
+      <Suspense fallback={<RecommendedProductsSkeleton />}>
+        <RecommendedProducts />
+      </Suspense>
+
+      <Ping />
+
+      <Suspense fallback={<ReviewsSkeleton />}>
+        <Reviews />
+      </Suspense>
+    </div>
+  );
+}
